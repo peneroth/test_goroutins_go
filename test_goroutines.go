7c92337f83fb05ac2e1beb416b8e4beaa72d69fa
Peter Eneroth <peter.eneroth@gmail.com>
2020-07-16 16:27:03 +0200

    Measure execution time

--- test_goroutines.go
+++ test_goroutines.go
@@ -9,18 +9,19 @@ import (
 func arbInc(i int, c chan float64) {
 	// Some stupid math to consume time
 	f := 1.0 / float64(i)
-	for j := 0; j < 20000; j++ {
+	for j := 0; j < 10000; j++ {
 		f = math.Sin(f) + 0.1
 	}
 	c <- f
 }
 
-const nbrRoutines = 1000000
+const nbrRoutines = 100000
 
 func main() {
 
 	c := make(chan float64)
 
+	start := time.Now()
 	for i := 0; i < nbrRoutines; i++ {
 		// fmt.Println("Create routine ", i)
 		go arbInc(i, c)
@@ -32,7 +33,9 @@ func main() {
 		x = x + 1
 		// fmt.Println("Recieved from routine", x-1)
 	}
+	duration := time.Since(start)
 	fmt.Println("All routines done")
-	time.Sleep(2 * time.Second)
+	fmt.Println("Duration =", duration)
+	time.Sleep(time.Second)
 	fmt.Println("Done")
 }
