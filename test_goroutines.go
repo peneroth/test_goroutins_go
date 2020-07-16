657252cd584a352f714a5c45c24544bd85af8469
Peter Eneroth <peter.eneroth@gmail.com>
2020-07-16 11:35:21 +0200

    Create high number of routins to maximize CPU load on all cores

--- test_goroutines.go
+++ test_goroutines.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
-func f(from string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(from, ":", i)
+func arbInc(i int, c chan float64) {
+	// Some stupid math to consume time
+	f := 1.0 / float64(i)
+	for j := 0; j < 20000; j++ {
+		f = math.Sin(f) + 0.1
 	}
+	c <- f
 }
 
+const nbrRoutines = 1000000
+
 func main() {
-	f("direct")
 
-	go f("go routine thread")
+	c := make(chan float64)
 
-	for i := 0; i < 3; i++ {
-		go func(msg string) {
-			fmt.Println(msg)
-		}("Main thread")
+	for i := 0; i < nbrRoutines; i++ {
+		// fmt.Println("Create routine ", i)
+		go arbInc(i, c)
 	}
+	fmt.Println("All routines created")
 
-	time.Sleep(time.Second)
-	fmt.Println("done")
+	for i := 0; i < nbrRoutines; i++ {
+		x := <-c
+		x = x + 1
+		// fmt.Println("Recieved from routine", x-1)
+	}
+	fmt.Println("All routines done")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
 }
